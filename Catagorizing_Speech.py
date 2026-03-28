d880d35c564c15bc70a7875e129a85d1e1bc274f
NRwave <pratyushkv.86@gmail.com>
2026-03-28 14:24:28 -0400

    Second Commit

---
+++ Catagorizing_Speech.py
@@ -0,0 +1,4 @@
+from constants import
+def clean_text(text):
+    lowercase_text = text.lower()
+    for()
---
+++ constants.py
@@ -0,0 +1,10 @@
+
+#Keywords for sentence detection
+INTERROGATIVE_KEYWORDS = ["who", "what", "when", "where", "why", "who", "is", "are", "do", "does", "can", "could", "would", "may"]
+EXCLAMATORY_KEYWORDS = ["wow", "awesome", "amazing", "crazy", "unbelievable", "yay", "woohoo", "oops", "whoops", "oopsie", "uh oh", "oh no", "weee", "aw, man", "ouch", "owie", "mmm", "yum", "yuck", "ick", "ew", "woah", "cool"]
+FILLER_WORDS = ["um", "like", "so", "uh", "you know", "you see"]
+#colors corresponding to each 
+question_color = 'B'
+statement_color = 'C'
+exclaimation_color = 'R'
+
